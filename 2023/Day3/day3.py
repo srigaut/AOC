29d440199e9b4d85478059ec0e4f8f48a28af195
Simon Rigaut <rigaut.simon@gmail.com>
2023-12-05 23:47:44 +0700

    DAY 3 - Committing result of part two

--- 2023/Day3/day3.py
+++ 2023/Day3/day3.py
@@ -23,7 +23,7 @@ def test_number(database, i_line, n_start, n_end):
     block = block.replace('.', '')
     return False if block == '' else True
 
-i_line = 0
+i_line_part1 = 0
 sum_engine = 0
 
 enlarged_database = enlarge_database(dataset)
@@ -31,8 +31,46 @@ enlarged_database = enlarge_database(dataset)
 for line in enlarged_database:
     numbers = re.finditer(r'\d+', line)
     for number in numbers:
-        if test_number(enlarged_database, i_line, number.start(), number.end()):
+        if test_number(enlarged_database, i_line_part1,
+                       number.start(), number.end()):
             sum_engine += int(number.group())
-    i_line += 1
+    i_line_part1 += 1
 
 print(sum_engine)
+
+### PART TWO ###
+
+# Function that multiplies valid gear for a given *
+def get_valid_numbers(database, i_line, n_start, n_end):
+    mul_gear = 1
+    star_numbers = []
+    for i in range(i_line - 1, i_line + 2):
+        numbers = re.finditer(r'\d+', database[i])
+        for number in numbers:
+            if number.end() >= n_start and number.start() <= n_end:
+                star_numbers.append(int(number.group()))
+            else:
+                pass
+    print(star_numbers)
+    if len(star_numbers) >= 2:
+        for elem in star_numbers:
+            mul_gear *= elem
+    else:
+        mul_gear = 0
+    print(mul_gear)
+    return mul_gear
+
+i_line_part2 = 0
+sum_gear = 0
+
+for line in enlarged_database:
+    stars = re.finditer(r'\*', line)
+    for star in stars:
+        mul_gear = get_valid_numbers(enlarged_database, i_line_part2,
+                                     star.start(), star.end())
+        print(mul_gear)
+        print(type(mul_gear))
+        sum_gear += mul_gear
+    i_line_part2 += 1
+
+print(sum_gear)

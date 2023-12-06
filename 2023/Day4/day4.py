27d189908caa67c756adcfbc842803bd3754d581
Simon Rigaut <rigaut.simon@gmail.com>
2023-12-06 10:50:21 +0700

    DAY 4 - Committing result of part two

--- 2023/Day4/day4.py
+++ 2023/Day4/day4.py
@@ -4,17 +4,46 @@ import re
 with open('adventofcode.com_2023_day_4_input.txt', 'r') as file:
     dataset = file.read().splitlines()
 
+# Functions that returns a tuple comprised of 2 lists (wining cards and
+# drawn cards lists)
+def winings_and_drawn_lists(line):
+    nb, wining_str, drawn_str = parse('Card {}: {} | {}', line)
+    wining_cards = re.findall(r'\d+', wining_str)
+    drawn_cards = re.findall(r'\d+', drawn_str)
+    return (wining_cards, drawn_cards)
+
 ### PART ONE ###
 
 total_worth = 0
 
-for line in dataset:
-    nb, wining_str, drawn_str = parse('Card {}: {} | {}', line)
-    wining_cards = re.findall(r'\d+', wining_str)
-    drawn_cards = re.findall(r'\d+', drawn_str)
+# Function that computes the number of wins for a given scratchcard
+def compute_wins(wining_cards, drawn_cards):
     nb_wins = 0
     for draw in drawn_cards:
         nb_wins += 1 if draw in wining_cards else 0
-    total_worth += 0 if nb_wins == 0 else 2 ** (nb_wins - 1)
+    return nb_wins
+
+for line in dataset:
+    wining_cards, drawn_cards = winings_and_drawn_lists(line)
+    nb_wins_per_line = compute_wins(wining_cards, drawn_cards)
+    total_worth += 0 if nb_wins_per_line == 0 else 2 ** (nb_wins_per_line - 1)
 
 print(total_worth)
+
+### PART TWO ###
+
+i_line = 0
+dataset_nb_instances = [1] * len(dataset)
+
+for line in dataset:
+    wining_cards, drawn_cards = winings_and_drawn_lists(line)
+    nb_wins_per_line = compute_wins(wining_cards, drawn_cards)
+    if nb_wins_per_line >= 1:
+        for add_scratchcards in range(1, nb_wins_per_line + 1):
+            dataset_nb_instances[i_line + add_scratchcards] += \
+                dataset_nb_instances[i_line]
+    else:
+        pass
+    i_line += 1
+
+print(sum(dataset_nb_instances))

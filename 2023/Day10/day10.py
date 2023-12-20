ef32626c447ebbfb6a6cd904713d37f81fa8724b
Simon Rigaut <rigaut.simon@gmail.com>
2023-12-20 16:56:35 +0800

    DAY 10 - Committing current code (not returning correct answer for part two)

--- 2023/Day10/day10.py
+++ 2023/Day10/day10.py
@@ -1,5 +1,7 @@
 import timer
 import pandas as pd
+import shapely
+import numpy as np
 
 with open('adventofcode.com_2023_day_10_input.txt', 'r') as file:
     data = file.read().splitlines()
@@ -34,15 +36,33 @@ End = False
 move1 = (start_row, start_column, 'N')
 move2 = (start_row, start_column, 'S')
 count = 0
+coordinates_list1 = []
+coordinates_list1.append((start_row, start_column))
+coordinates_list2 = []
 
 while not End:
     move1 = move(move1[0], move1[1], move1[2])
     move2 = move(move2[0], move2[1], move2[2])
+    coordinates_list1.append((move1[0], move1[1]))
+    coordinates_list2.append((move2[0], move2[1]))
     count += 1
     if move1[0] == move2[0] and move1[1] == move2[1]:
         End = True
+        coordinates_list1.pop()
         break
 
 print(count)
 
 Timer.get_time(1)
+
+coordinates_list2.reverse()
+coord = coordinates_list1 + coordinates_list2 + [(start_row, start_column)]
+
+poly = shapely.Polygon(coord)
+
+list_to_check = np.array([shapely.Point(d[0],d[1]) for d in df[df.isin(['.'])].stack().index.to_list()])
+
+result = shapely.contains(poly, list_to_check)
+print(sum(result))
+
+Timer.get_time(2)
